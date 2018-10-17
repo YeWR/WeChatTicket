d1088d5d79e48d82754c3a0a80366c04cd550611
wuhaixu2016 <wuhaix2016@163.com>
2018-10-17 14:39:11 +0800

    test acti

--- adminpage/test.py
+++ adminpage/test.py
@@ -5,9 +5,10 @@ from . import views as views
 # import requests
 import json
 
+c = Client(HTTP_USER_AGENT='Mozilla/5.0')
 class AuthLoginGet(TestCase):
     def test_admin_login_get(self):
-        c = Client()
+        # c = Client(HTTP_USER_AGENT='Mozilla/5.0')
         resp = c.get('/api/a/login')
         mess = json.loads(str(resp.content, encoding = "utf-8"))
         self.assertEqual(mess['code'], 4)
@@ -15,7 +16,7 @@ class AuthLoginGet(TestCase):
 
 class AuthLoginPostFail(TestCase):
     def test_admin_login_fail(self):
-        c = Client()
+        # c = Client(HTTP_USER_AGENT='Mozilla/5.0')
         resp = c.post('/api/a/login',{'username':'wu', 'password':'1234'})
         mess = json.loads(str(resp.content, encoding = "utf-8"))
         self.assertEqual(mess['code'], 3)
@@ -23,61 +24,36 @@ class AuthLoginPostFail(TestCase):
 
 class AuthLoginPostSucc(TestCase):
     def test_admin_login_success(self):
-        c = Client()
+        # c = Client(HTTP_USER_AGENT='Mozilla/5.0')
         User.objects.create_user(username='wu',email="dui_zhang@163.com",password='1234')
         resp = c.post('/api/a/login',{'username':'wu', 'password':'1234'})
         mess = json.loads(str(resp.content, encoding = "utf-8"))
         self.assertEqual(mess['code'], 0)
         self.assertEqual(mess['msg'], '')
 
-# class AuthLogoutPost(TestCase):
-#     def test_admin_logout(self):
-#         c = Client()
-#         resp = c.post('/api/a/logout')
-#         mess = json.loads(str(resp.content, encoding = "utf-8"))
-#         self.assertEqual(mess['code'], 0)
-#         self.assertEqual(mess['msg'], '')
+class AuthLogoutPost(TestCase):
+    def test_admin_logout(self):
+        resp = c.post('/api/a/logout',{'arg':'useless'})
+        mess = json.loads(str(resp.content, encoding = "utf-8"))
+        self.assertEqual(mess['code'], 0)
+        self.assertEqual(mess['msg'], '')
 
-# class ActiListGetFail(TestCase):
-#     def test_acti_get_fail(self):
-#         c = Client()
-#         resp = c.post('/api/a/activity/list')
-#         mess = json.loads(str(resp.content, encoding = "utf-8"))
-#         self.assertEqual(mess['code'], 4)
-#         self.assertEqual(mess['msg'], '')
+class ActiListGetFail(TestCase):
+    def test_acti_get_fail(self):
+        resp = c.get('/api/a/activity/list')
+        mess = json.loads(str(resp.content, encoding = "utf-8"))
+        self.assertEqual(mess['code'], 4)
+        self.assertEqual(mess['msg'], '')
 
-# class ActiListGetSucc(TestCase):
-#
-#     def test_acti_get_succ(self):
-#         url = 'http://140.143.17.33/api/a/login'
-#         parms = {'username':'wuhaixu', 'password':'1234qwer'}
-#         headers = {
-#         'User-agent':'Mozilla/5.0'
-#         }
-#         resp = requests.post(url,data=parms,headers=headers)
-#         mess = json.loads(resp.text)
-#         self.assertEqual(mess['code'], 0)
-#         self.assertEqual(mess['msg'], '')
-#
-#         url = 'http://140.143.17.33/api/a/activity/list'
-#         parms = {}
-#         headers = {
-#             'User-agent': 'Mozilla/5.0'
-#         }
-#         resp = requests.get(url, data=parms, headers=headers)
-#         mess = json.loads(resp.text)
-#         print ('_____',resp.text)
-#         self.assertEqual(mess['code'], 0)
-#         self.assertEqual(mess['msg'], '')
+class ActiListGetSucc(TestCase):
 
-# class ActiAdminPageTest(TestCase):
-#     def test_admin_list(self):
-#         url = 'http://140.143.17.33/api/a/login'
-#         parms = {}
-#         headers = {
-#             'User-agent': 'Mozilla/5.0'
-#         }
-#         resp = requests.get(url, data=parms, headers=headers)
-#         mess = json.loads(resp.text)
-#         self.assertEqual(mess['code'], 4)
-#         self.assertEqual(mess['msg'], '')
+    def test_acti_get_succ(self):
+        User.objects.create_user(username='wu', email="dui_zhang@163.com", password='1234')
+        c.login(username='wu',password='1234')
+        resp = c.get('/api/a/activity/list')
+        mess = json.loads(str(resp.content, encoding = "utf-8"))
+        self.assertEqual(mess['code'], 0)
+        self.assertEqual(mess['msg'], '')
+        # return empty list
+        self.assertEqual(mess['data'],[])
+
