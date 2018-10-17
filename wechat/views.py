feea6f66287e7f12a34c233e1e4a3ab07c867681
YeWR <ywr16@mails.tsinghua.edu.cn>
2018-10-18 01:54:49 +0800

    Handler处理完，数据库还需要加锁，还需要写测试。

--- wechat/views.py
+++ wechat/views.py
@@ -11,7 +11,8 @@ class CustomWeChatView(WeChatView):
     lib = WeChatLib(WECHAT_TOKEN, WECHAT_APPID, WECHAT_SECRET)
 
     handlers = [
-        HelpOrSubscribeHandler, UnbindOrUnsubscribeHandler, BindAccountHandler, BookEmptyHandler,
+        HelpOrSubscribeHandler, UnbindOrUnsubscribeHandler, BindAccountHandler,
+        BookEmptyHandler, BookWhatHandler, CheckTicketHandler, BookTicketHandler, RefundTicketHandler
     ]
     error_message_handler = ErrorHandler
     default_handler = DefaultHandler
